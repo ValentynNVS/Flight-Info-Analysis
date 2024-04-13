4b7259928673e1233516955e214798f36555c3a1
ValentynNVS <novosidlak3@gmail.com>
2024-04-13 14:24:09 -0400

    This is the first commit to my project. I created a file header. Also, for my project I will assume that the maximum number of flight to be read are 100.

---
+++ Git project/mainFile.cpp
@@ -0,0 +1,14 @@
+/*
+* FILE : mainFile.cpp
+* PROGRAMMER : Valentyn Novosydliuk
+* FIRST VERSION : 04/13/2024
+* DESCRIPTION :
+* This program reads the information from the text document and 
+* displays the least fare of the flights. 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const int kCharMaxLenghts = 100;
